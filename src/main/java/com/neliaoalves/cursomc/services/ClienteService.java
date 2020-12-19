492cfb83470b55cfb78db08938f421e192841df4
joao.romao <joao.romao@dataprev.gov.br>
2020-12-19 19:41:10 -0300

    Enviando imagem via endpoint

--- src/main/java/com/neliaoalves/cursomc/services/ClienteService.java
+++ src/main/java/com/neliaoalves/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.neliaoalves.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,6 +11,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.neliaoalves.cursomc.domain.Cidade;
 import com.neliaoalves.cursomc.domain.Cliente;
@@ -37,6 +39,8 @@ public class ClienteService {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private S3Service s3Service;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -101,5 +105,9 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 }
